b1dd153d1c59c205dd886a8f357bc750c173d02b
luizteles6 <lgustavost.10@gmail.com>
2026-06-04 00:19:04 -0300

    Update questao01.c

--- questao01/questao01.c
+++ questao01/questao01.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main() {
+
+    // Declaração das variáveis
     int idade = 17;
     float altura = 1.75;
     char inicial = 'L';
 
+    // Exibição dos dados na tela
     printf("Idade: %d\n", idade);
     printf("Altura: %.2f\n", altura);
     printf("Inicial: %c\n", inicial);
