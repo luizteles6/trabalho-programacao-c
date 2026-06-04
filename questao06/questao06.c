57ac0f758f5192ac8f6636cb698e290bba2589a2
luizteles6 <lgustavost.10@gmail.com>
2026-06-04 00:25:22 -0300

    Create questao06.c

---
+++ questao06/questao06.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+    // Declaração das variáveis
+    char nome[50];
+    char cidade[50];
+    int idade;
+
+    // Entrada dos dados
+    printf("Digite seu nome: ");
+    scanf(" %[^\n]", nome);
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite sua cidade: ");
+    scanf(" %[^\n]", cidade);
+
+    // Exibição dos dados
+    printf("\nNome: %s\n", nome);
+    printf("Idade: %d\n", idade);
+    printf("Cidade: %s\n", cidade);
+
+    return 0;
+}
