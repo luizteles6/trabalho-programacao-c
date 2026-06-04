e4b4fd9208eec32f6710c389e91df8063c25632e
luizteles6 <lgustavost.10@gmail.com>
2026-06-04 00:15:26 -0300

    Create questao01.c

---
+++ questao01/questao01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 17;
+    float altura = 1.75;
+    char inicial = 'L';
+
+    printf("Idade: %d\n", idade);
+    printf("Altura: %.2f\n", altura);
+    printf("Inicial: %c\n", inicial);
+
+    return 0;
+}
