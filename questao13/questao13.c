22288ed7637a3e7ff26b001a3bf03bf15fbf7c8b
luizteles6 <lgustavost.10@gmail.com>
2026-06-04 00:28:47 -0300

    Create questao13.c

---
+++ questao13/questao13.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+
+    // Declaração da variável
+    int i;
+
+    // Contagem de 1 a 10
+    for(i = 1; i <= 10; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
